d3a74963c77aed759461fa95fab650f2e8b354cc
Prashilkumar <rupaparapa@gmail.com>
2023-06-17 13:21:51 +0200

    data structure DictOfValidOutput is added

---
+++ data_structures.py
@@ -0,0 +1,12 @@
+class DictOfValidOutput(dict):
+	'''
+	- this custom dictionary is made to control the output valúes of the gates
+	- valid output of the gates can be one of the value from 'valid_outputs' list
+	'''
+	def __setitem__(self, key, value):
+		valid_outputs = ['0', '1', 0, 1]
+		if value in valid_outputs:
+			value = int(value)
+			super().__setitem__(key, value)
+		else:
+			raise ValueError('Only these values are allowed as output of the gate: ', valid_outputs)
